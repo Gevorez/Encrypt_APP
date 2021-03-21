f0db999a5f1357be36e03e2c3d138038b846704e
Kamil Niewiarowski <niewiarowski.kamil.98@gmail.com>
2021-03-21 23:42:31 +0100

    Gevorez
    Files

---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BSK_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.CharacterCasing = CharacterCasing.Upper;
+        }
+
+        private void szyfrXor()
+        {
+            string tekst = textBox2.Text;
+
+            char[] msg = tekst.ToCharArray();
+
+            char[] key = textBox1.Text.ToCharArray();
+
+            int i = 0;
+
+            string wynik = "";
+
+            for (int j = 0; j < tekst.Length; j++)
+            {
+                char c = (char)((int)msg[j] ^ (int)key[i++ % key.Length] ^ (int)key[i++ % key.Length]);
+                wynik = wynik + c;
+            }
+
+            textBox3.Text = wynik;
+        }
+
+        private void szyfrCezar()
+        {
+            string input = textBox2.Text;
+
+            int shift = int.Parse(textBox4.Text);
+
+            char[] alphabet = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            string wynik2 = "";
+
+            char[] input_array = input.ToCharArray();
+
+            for (int j = 0; j < input_array.Length; j++)
+            {
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    if (input_array[j] == alphabet[i])
+                    {
+                        input_array[j] = alphabet[(i + shift) % alphabet.Length];
+                        break;
+                    }
+                }
+            }
+
+            wynik2 = new string(input_array);
+
+            textBox3.Text = wynik2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RadioButton rb1 = radioButton1;
+            RadioButton rb2 = radioButton2;
+
+            if (rb1.Checked)
+            {
+                szyfrXor();
+                rb2.Checked = false;
+            }
+
+            if (rb2.Checked)
+            {
+                szyfrCezar();
+                rb1.Checked = false;
+                textBox1.Text = String.Empty;
+
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb2 = radioButton2;
+            if (rb2.Checked)
+            {
+                textBox1.Text = String.Empty;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb1 = radioButton1;
+            if (rb1.Checked)
+            {
+                textBox4.Text = String.Empty;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("BSK_Lab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BSK_Lab3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("68889c0d-70e1-4847-8d92-c9c560e7d977")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
